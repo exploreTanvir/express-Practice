f83076538220d5c8051560ad75dd1567a0a4d9b0
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-08-12 07:14:11 +0600

    Route

---
+++ Route/route.js
@@ -0,0 +1,23 @@
+//Browser only access Get() not other
+//T0 access other we should be used postman to test
+
+
+
+var express=require("express")
+app=express()
+app.get("/",function(req,res){
+    res.send("Hello Express This is Home")
+})
+app.get("/contact",function(req,res){
+    res.send("Hello Express This is contact")
+})
+app.get("/about",function(req,res){
+    res.send("Hello Express This is About")
+})
+app.post("/portfolio",function(req,res){
+    res.send("Hello Express This is Portfolio")
+})
+
+app.listen(8001,function(){
+    console.log("run success")
+})
