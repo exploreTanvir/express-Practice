57b452e49acb6aeff8e51fcba37e5db9982d0d87
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-09-13 22:32:13 +0600

    Add files via upload

--- mongo with express/index.js
+++ mongo with express/index.js
@@ -1,22 +1,28 @@
 const express=require("express")
 const app=new express()
 const mongoose=require("mongoose")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-//Technic one  use then,catch
-
-mongoose.connect('mongodb://127.0.0.1:27017/Student')
-.then(()=>console.log("db is connected"))
-.catch((error)=>{
-    console.log("db is not connected")
-    console.log(error)
-    process.exit(1)
+app.get("/",(req,res)=>{
+  res.send("Welcome to the server")
 })
 
-//Technic Two us async,await
+//Technic one  use then,catch for connect with database
+
+// mongoose.connect('mongodb://127.0.0.1:27017/Schools')
+// .then(()=>console.log("db is connected"))
+// .catch((error)=>{
+//     console.log("db is not connected")
+//     console.log(error)
+//     process.exit(1)
+// })
+
+//Technic Two us async,await for connect with database
 
 const ConnectDB=async()=>{
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Student')
+    await mongoose.connect('mongodb://127.0.0.1:27017/Schools')
     console.log("db is connected")
   } catch (error) {
     console.log("db is not connected")
@@ -27,10 +33,52 @@ const ConnectDB=async()=>{
 }
 
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to the server")
+//Create product Schema
+
+const productSchema= new mongoose.Schema({
+  title:{
+          type:String,
+          required:true
+        },
+  price:{
+    type:Number,
+    required:true
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  date:{
+    type:Date,
+    default:Date.now
+  }
 })
-var port=3004
+
+
+//Create product Model
+const MyModel=mongoose.model("Teachers",productSchema)
+
+
+// Save data in Database
+
+app.post("/products",async(req,res)=>{
+
+  try { 
+   const newDBModel=new MyModel({
+     //get data from req body
+    title:req.body.title,
+    price:req.body.price,
+    description:req.body.description
+   })
+
+   const productData=await newDBModel.save()
+   req.status(201).send(productData)
+  } catch (error) {
+    res.status(500).send({message:error.message})
+  }
+})
+
+var port=3017
 app.listen(port,async()=>{
     console.log("Server run success on port number "+ port);
     // for Technic two
