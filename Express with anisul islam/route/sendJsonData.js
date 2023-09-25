62a11a8190908cce4698fcc8a16338143e472f4b
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-09-25 21:41:39 +0600

    Express JS with anisul islam

--- Express with anisul islam/route/sendJsonData.js
+++ Express with anisul islam/route/sendJsonData.js
@@ -4,6 +4,18 @@ const bodyParser=require("body-parser")
 router.use(bodyParser.urlencoded({extended:false}))
 router.use(bodyParser.json())
 
+// send data from html form
+router.get("/register",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+router.post("/register",(req,res)=>{
+    const fullName=req.body.fullName
+    const age=req.body.age
+   res.send(`<h1>Student full name is : ${fullName} and age is : ${age}</h1>`)
+})
+
+// Send json data from body request
 router.post("/json",(req,res)=>{
    const name=req.body.name
    const roll=req.body.roll
