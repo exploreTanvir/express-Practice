2978b5cc437e46e120caefcfd51eff1edfd84db9
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-01-30 08:10:56 +0600

    Add files via upload

---
+++ first-api/index.js
@@ -0,0 +1,11 @@
+const express=require("express")
+app=express()
+
+app.get("/",(req,res)=>{
+    res.send("hello express js")
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})
