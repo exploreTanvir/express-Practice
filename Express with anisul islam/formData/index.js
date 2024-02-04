cf22e4d4e5b8d478a9f348454b10b6f160ac05b3
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-02-04 23:19:01 +0600

    Add files via upload

---
+++ Express with anisul islam/formData/index.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser")
+const express=require("express")
+app=express()
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.post("/user",(req,res)=>{
+    const name=req.body.name
+    const age=req.body.age
+    // res.send("hello express js")
+    res.send(`user name is ${name} and age is ${age}`)
+})
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})
+
+
+
+
