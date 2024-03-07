43ad1824b6a9fabef21cb916ab9b68764b0c7d70
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-03-07 16:37:56 +0600

    Add files via upload

--- Express with anisul islam/DB/index.js
+++ Express with anisul islam/DB/index.js
@@ -2,24 +2,38 @@ const express=require("express")
 const mongoose=require("mongoose")
 const app=express()
 
+// create schema
+const ProductSchema=mongoose.Schema({
+    title:{
+        typeof:String,
+    },
+    price:Number,
+    desc:String,
+    createdTime:{
+        typeof:Date,
+    }
+})
+
+
+//model
+const productModel=mongoose.model("product",ProductSchema)
+
 const connectDB=async()=>{
  
 try {
-    await  mongoose.connect("mongodb://127.0.01:27017/students")
+    await  mongoose.connect('mongodb://127.0.0.1:27017/hello');
     console.log("Connected")
 } catch (error) {
     console.log("not connect")
-    console.log(error.message)
+    
     process.exit(1)
 }
 
 }
 
 
-
-
 app.listen(3002,async()=>{
-    console.log("server is running on the port number 3000")
+    console.log("server is running on the port number 3002")
     await connectDB()
 })
 
